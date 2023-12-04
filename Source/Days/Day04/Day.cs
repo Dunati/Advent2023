c76e5e0ffd83f559af6f5f613afa53ff60ae0580
Dolphin Hawkins <dolphin@exitzer0.com>
2023-12-04 09:28:41 +0000

    Day 4 part 2

--- Source/Days/Day04/Day.cs
+++ Source/Days/Day04/Day.cs
@@ -5,6 +5,40 @@ class Day : BaseDay
 {
     public override string Run(int part, string rawData)
     {
+        if (part == 1)
+        {
+            return Part1(rawData);
+        }
+
+        var lines = rawData.Lines().ToArray();
+        int[] cards = new int[lines.Length];
+        Array.Fill(cards, 1);
+
+        for(int i = 0; i < cards.Length; i++) 
+        {
+            string line = lines[i];
+            string[] parts = line.Split(':', '|');
+            var winning = new Dictionary<int, int>(parts[1].ToInts(10, " ").Select(x => new KeyValuePair<int, int>(x, 0)));
+
+            foreach (int number in parts[2].ToInts(10, " "))
+            {
+                if (winning.TryGetValue(number, out int value))
+                {
+                    winning[number] = value + 1;
+                }
+            }
+            int matches = winning.Values.Where(x => x > 0).Count();
+            for(int m = 1; m<=matches; m++) {
+                if(m+i < cards.Length)
+                {
+                    cards[m+i] +=cards[i];
+                }
+            }
+        }
+        return cards.Sum().ToString();
+    }
+    private static string Part1(string rawData)
+    {
         int total_points = 0;
         foreach (var line in rawData.Lines())
         {
