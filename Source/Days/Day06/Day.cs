2420bb39604a6927383b52f0e52fb12f0291dad5
Dolphin Hawkins <dolphin@exitzer0.com>
2023-12-06 10:51:44 +0000

    Day 6 Part 1 simple

--- Source/Days/Day06/Day.cs
+++ Source/Days/Day06/Day.cs
@@ -3,6 +3,29 @@ namespace Day06;
 
 class Day : BaseDay {
     public override string Run(int part, string rawData) {
-        return "";
+
+        var (time, distance, rest) = rawData.Lines().Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).ToInts().ToArray());
+        Debug.Assert(time.Length == distance.Length);
+
+        int ways = 1;
+        for(int i = 0; i < time.Length;i++)
+        {
+            int t = time[i];
+            int d = distance[i];
+            int wins = 0;
+            for(int ms=1;ms<t-1; ms++)
+            {
+                int end = ms * (t - ms);
+                if(end > d)
+                {
+                    wins++;
+                }                
+            }
+            if(wins > 0)
+            {
+                ways *= wins;
+            }
+        }
+        return ways.ToString();
     }
 }
