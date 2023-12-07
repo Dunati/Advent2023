25fe2e2f8e985737e8fc01e268d1836b230e064d
Dolphin Hawkins <dolphin@exitzer0.com>
2023-12-07 14:26:25 +0000

    7-2

--- Source/Days/Day07/Day.cs
+++ Source/Days/Day07/Day.cs
@@ -8,6 +8,11 @@ class Day : BaseDay
         public string cards;
         public int bid;
         public Hands type;
+
+        public override string ToString()
+        {
+            return $"{cards} {type} {bid}";
+        }
     }
     enum Hands
     {
@@ -56,10 +61,123 @@ class Day : BaseDay
     }
     public override string Run(int part, string rawData)
     {
-        if(part == 1)
-        return Part1(rawData);
+        if (part == 1)
+            return Part1(rawData);
 
-        return "";
+        order['J'] = 1;
+
+        List<Hand> hands = new List<Hand>();
+        foreach (var line in rawData.Lines())
+        {
+
+            DefaultDictionary<char, int> hand = new DefaultDictionary<char, int>();
+            for (int i = 0; i < 5; i++)
+            {
+                hand[line[i]]++;
+            }
+            Hands type = 0;
+            int jokers = hand['J'];
+            switch (hand.Count)
+            {
+            case 1: type = Hands.Five; break;
+            case 2:
+                if (jokers > 0)
+                {
+                    type = Hands.Five;
+                }
+                else if (hand.First().Value == 1 || hand.First().Value == 4)
+                {
+                    type = Hands.Four;
+                }
+                else
+                {
+                    type = Hands.FullHouse;
+                }
+
+                break;
+            case 3:
+                if (hand.Values.Max() == 3)
+                {
+                    if(jokers == 3)
+                    {
+                        type = Hands.Four;
+                    }
+                    else if (jokers == 2)
+                    {
+                        type = Hands.Five;
+                    }
+                    else if (jokers == 1)
+                    {
+                        type = Hands.Four;
+                    }
+                    else
+                    {
+                        Debug.Assert(jokers == 0);
+                        type = Hands.Three;
+
+                    }
+                }
+                else
+                {
+                    if (jokers == 2)
+                    {
+                        type = Hands.Four;
+                    }
+                    else if (jokers == 1)
+                    {
+                        type = Hands.FullHouse;
+                    }
+                    else
+                    {
+                        Debug.Assert(jokers == 0);
+                        type = Hands.TwoPair;
+                    }
+                }
+
+                break;
+            case 4:
+                if (jokers > 0)
+                {
+                    type = Hands.Three;
+                }
+                else
+                {
+                    type = Hands.OnePair;
+                }
+                break;
+            default:
+                Debug.Assert(hand.Count == 5);
+                if (jokers > 0)
+                {
+                    type = Hands.OnePair;
+                }
+                else
+                {
+
+                    type = Hands.HighCard;
+                }
+                break;
+            }
+
+            hands.Add(new Hand() { cards = line[0..5], type = type, bid = int.Parse(line[6..]) });
+        }
+
+        hands.Sort(OrderHand);
+
+        Trace.WriteLine("");
+        Trace.WriteLine("");
+        decimal score = 0;
+        decimal rank = 1;
+        foreach (Hand hand in hands)
+        {
+            Trace.WriteLine(hand);
+            score += rank * hand.bid;
+            rank++;
+        }
+
+        // 251005995 too low
+        // 251195607
+        return score.ToString();
     }
 
     private static string Part1(string rawData)
